ee058f8c97eb5ce5296f076894feb071d951899a
Charles Louisin <clynlouisin@gmail.com>
2024-10-18 17:30:28 +0100

    Done

---
+++ main.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test1-sep-2024')
+.then(() => {
+    
+    const PORT = 3000;
+    const app = express();
+    app.use(express.json());
+
+    const indexRoute = require('./routes/index.route');
+    const coursRoute = require('./routes/index.route');
+
+    app.use('/api', indexRoute);
+    app.use('/courses', coursRoute);
+
+    app.listen(3000, () => console.log('Application listening on http://localhost:' + PORT));
+})
+.catch(err => {
+    console.error(err)
+});
